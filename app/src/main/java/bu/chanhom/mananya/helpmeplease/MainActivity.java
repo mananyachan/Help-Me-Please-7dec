2fed4235b63f9b5225ea2343fbe30bfe35dd914c
masterUNG <phrombutr@gmail.com>
2016-12-07 14:45:49 +0700

    Cursor

--- app/src/main/java/bu/chanhom/mananya/helpmeplease/MainActivity.java
+++ app/src/main/java/bu/chanhom/mananya/helpmeplease/MainActivity.java
@@ -1,8 +1,11 @@
 package bu.chanhom.mananya.helpmeplease;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -32,8 +35,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Create ListView
+        createListview();
+
 
     } //Main Method
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        createListview();
+    }
+
+    private void createListview() {
+
+        try {
+
+            SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                    MODE_PRIVATE, null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM helpTABLE WHERE Favorite = 1", null);
+            cursor.moveToFirst();
+            Log.d("7decV3", "cursor.count ==> " + cursor.getCount());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }   // createListView
 
 } // Main Class
 
