bf080e2ee05a1b3b5313a94f315d9ea67e0a138a
masterUNG <phrombutr@gmail.com>
2017-02-11 10:01:25 +0700

    add Calculate OK

--- app/src/main/java/bu/chanhom/mananya/helpmeplease/MainActivity.java
+++ app/src/main/java/bu/chanhom/mananya/helpmeplease/MainActivity.java
@@ -79,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
         Log.d("7decV4", "lat ==> " + latADouble);
         Log.d("7decV4", "lng ==> " + lngADouble);
 
+        //Calculate All Point Where action = 0
+
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -90,6 +92,29 @@ public class MainActivity extends AppCompatActivity {
 
 
     }   // myLoop
+
+
+    //นี่คือ เมทอด ที่หาระยะ ระหว่างจุด
+    private static double distance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515 * 1.609344;
+
+
+        return (dist);
+    }
+
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
+    }
+
+
 
     @Override
     protected void onStop() {
